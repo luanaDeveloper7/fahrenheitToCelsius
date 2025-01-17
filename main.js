3bda206527e365fdc6843365de03339a67a06fc8
Luana <luana.developer7@gmail.com>
2025-01-17 11:08:15 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const submitButton =  document.querySelector(".submitButton");
+let celsiusTemperatureText = document.querySelector(".celsiusValue");
+submitButton.addEventListener("click", convertFahrenheintInCelsius);
+
+document.addEventListener("keydown", (keyDownEvent) =>{
+    if(keyDownEvent.key === "Enter"){
+        convertFahrenheintInCelsius();
+    }
+})
+
+function convertFahrenheintInCelsius(){
+    let fahrenheitTemperature = parseFloat(document.querySelector(".fahrenheitTemperature").value);
+    if(isNaN(fahrenheitTemperature)){
+        alert("Write a temperature!");
+        return;
+    }else{
+        let celsiusTemperature =  (( ( fahrenheitTemperature - 32 ) * 5 ) / 9).toFixed(2);
+        celsiusTemperatureText.textContent = `${fahrenheitTemperature} ºF is ${celsiusTemperature} ºC`;
+        let input = document.querySelector("input");
+        input.value = "";
+    }
+    
+}
